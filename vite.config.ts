cdd2e677c6a3b982796b1ff3d531e375e99b605d
fancyhermit <sahejyot.bhatia@gmail.com>
2026-01-17 21:21:22 +0530

    ka

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: '/',
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 });
